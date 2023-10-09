2d51ce08f498eec837143142496b9f8723abbbc9
Mohamed Gomaa <ph.dude.95@gmail.com>
2023-10-08 17:26:00 -0700

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+ *main - entry point
+ *Description: gives the alphabet in lowercase
+ *Return: 0 (success)
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
