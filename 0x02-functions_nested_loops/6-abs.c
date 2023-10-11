cc8466da2c73645f2141452ad4b61327d68b609b
Mohamed Gomaa <ph.dude.95@gmail.com>
2023-10-10 20:45:42 -0700

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_abs - entry point
+ *Description: checks if the alphabet in lowercase
+ *@n: the input of the func
+ *Return: 0 (lowercase) & 1 (otherwise)
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = (-1) * n;
+	}
+	return (n);
+}
