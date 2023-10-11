c2e771732c2c1ecc5018207eedda46cf87cdddc1
Mohamed Gomaa <ph.dude.95@gmail.com>
2023-10-10 19:22:21 -0700

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *Description: gives the alphabet in lowercase
+ *Return: 0 (success)
+ */
+
+int main(void)
+{
+printf("_putchar\n");
+return (0);
+}
