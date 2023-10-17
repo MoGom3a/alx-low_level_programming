0684f353d0a3a129fd78d663a6b03b30060d08f1
Mohamed Gomaa <ph.dude.95@gmail.com>
2023-10-16 17:18:20 -0700

    7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *puts_half - takes an array of int
+ *
+ *@str: pointer for the int
+ *
+ *Return: void
+ */
+
+void puts_half(char *str)
+{
+	int l;
+
+	for (l = 0; str[l] != '\0'; l++)
+		;
+	l = l / 2;
+	while (str[l] != '\0')
+	{
+		_putchar(str[l]);
+			l++;
+	}
+	_putchar('\n');
+}
