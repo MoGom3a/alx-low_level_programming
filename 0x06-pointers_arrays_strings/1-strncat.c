8f447384be676149383132ab614e362f7e28ed57
Mohamed Gomaa <ph.dude.95@gmail.com>
2023-10-19 15:44:49 -0700

    13

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *_strcat - function to concatinate 2 str
+ *
+ *@dest: the destination
+ *@src: the source for input
+ *@n: the n of bytes of src
+ *
+ * Return: Pointer to resulting str
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int c, i;
+
+	c = 0;
+
+	while (dest[c])
+		c++;
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[c + i] = src[i];
+	dest[c + 1] = '\0';
+	return (dest);
