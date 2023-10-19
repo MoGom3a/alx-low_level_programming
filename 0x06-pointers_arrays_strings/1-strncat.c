2d11433d0c56fcb66da18bb719ef303bebf42b3d
Mohamed Gomaa <ph.dude.95@gmail.com>
2023-10-19 16:02:06 -0700

    1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,17 +12,11 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int c, i;
+	int index = 0, dest_len = 0;
 
-	c = 0;
-
-	while (dest[c])
-		c++;
-
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[c + i] = src[i];
-
-	dest[c + 1] = '\0';
-
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
 	return (dest);
 }
