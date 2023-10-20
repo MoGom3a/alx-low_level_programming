f62d8fa9469b6c650c954c81fc64139b28eb097d
Mohamed Gomaa <ph.dude.95@gmail.com>
2023-10-19 18:48:10 -0700

    4

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *leet - function to concatinate 2 str
+ *@c : The input string to be modified.
+ * Return: Pointer to resulting str
+ */
+
+char *leet(char*c)
+{
+	char *cp = c;
+	char key[] = {'A', 'E', 'O', 'T', 'L'};
+	int value[] = {4, 3, 0, 7, 1};
+
+	while (*c)
+	{
+		for (i = 0; i < sizeof(key) / sizeof(char); i++)
+		{
+			if (*c == key[i] || *c == key [i] + 32)
+			{
+				*c = 48 + value[i];
+			}
+		}
+		c++;
+	}
+
+	return (cp);
+}
