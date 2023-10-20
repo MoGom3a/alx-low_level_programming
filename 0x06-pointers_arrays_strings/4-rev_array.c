426402e54fea528a69f835d020cf9f0ae6c8f232
Mohamed Gomaa <ph.dude.95@gmail.com>
2023-10-19 17:37:38 -0700

    4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *_reverse_array - function to concatinate 2 str
+ *reverse_array - function to concatinate 2 str
  *
  *@a: the destination
  *@n: the source for input
@@ -13,7 +13,7 @@ void reverse_array(int *a, int n)
 {
 	int index, j;
 
-	j = n-1;
+	j = n - 1;
 
 	for (index = 0; index < j / 2; index++)
 	{
