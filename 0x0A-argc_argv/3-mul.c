fa0270d8103defc579e3a971e6403fb70131aeff
Mohamed Gomaa <ph.dude.95@gmail.com>
2023-10-27 17:46:08 -0700

    2

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ *main - take an array of char values and points to it
+ *@argc: arg counter
+ *@argv: arg value
+ *
+ *Return: 0 success
+ */
+
+int main(int argc, char *argv[])
+{
+	int total;
+
+	if (argc == 3)
+	{
+		total = atoi(argv[1] * atoi(argv[2]);
+		printf("%d\n", total);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	return (0);
+}
