a8f53bbc4dd6008a7d7624363bdbec0b8f9004c2
Mohamed Gomaa <ph.dude.95@gmail.com>
2023-10-27 18:15:58 -0700

    5

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - take an array of char values and points to it
+ *@argc: arg counter
+ *@argv: arg value
+ *
+ *Return: 0 success
+ */
+
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	char *c;
+
+	while (--argc)
+	{
+		for (c = argv[argc]; *c; c++)
+			if (*c < '0' || *c > '9')
+				return (printf("Error\n"), 1);
+		sum += atoi(argv[argc]);
+	}
+	printf("%d\n", sum);
+	return (0);
+	
+}
