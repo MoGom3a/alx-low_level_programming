abd1aef0dbf9c959fde02fc81cfec357e6066e24
Mohamed Gomaa <ph.dude.95@gmail.com>
2023-11-01 18:38:11 -0700

    5

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -24,12 +24,14 @@ char *argstostr(int ac, char **av)
 	if (str == NULL)
 		return (NULL);
 	for (i = 0; i < ac; i++)
+	{
 		for (n = 0; av[i][n]; n++)
 		{
 			str[r] = av[i][n];
 			r++;
 		}
-	if (str[r] == '\0')
-		str[r++] = '\n';
+		if (str[r] == '\0')
+			str[r++] = '\n';
+	}
 return (str);
 }
