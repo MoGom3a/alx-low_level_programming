8fad50269ca985a0fbf14ce107ce5d6c319f08cc
Mohamed Gomaa <ph.dude.95@gmail.com>
2023-11-01 18:22:47 -0700

    4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - frees the grid
+ * @grid: 2d arrey values
+ * @height: int
+ * return: NULL
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i = 0;
+
+	for (i; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
