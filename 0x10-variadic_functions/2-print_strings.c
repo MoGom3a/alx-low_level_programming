09387b21d0ca2fd224d828dc4e81783f113a1efc
Mohamed Gomaa <ph.dude.95@gmail.com>
2023-11-10 18:02:33 -0800

    2

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - sum of all args
+ * @separator: the str sep
+ * @n: counter
+ * @...: the args
+ *
+ * Return: the nums
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	int i = n;
+	char = *str;
+	va_list args;
+
+	if (!n)
+	{
+		printf("\n");
+		return;
+	}
+	va_start(args, n);
+	while (i--)
+		printf("%s%s",(str = va_arg(args, char *)) ? str : "(nil)",
+				i ? (separator ? separator : "") : "\n");
+	va_end(args);
+}
