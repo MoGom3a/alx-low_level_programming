36ad74d65aa69392ce3ff78fda394a3b401c9ffe
Mohamed <ph.dude.95@gmail.com>
2023-12-01 04:42:20 +0200

    0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,21 +10,27 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t rd, wr;
+	ssize_t nrd, nwr;
 	char *buf;
 
 	if (!filename)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
+
 	if (fd == -1)
 		return (0);
+
 	buf = malloc(sizeof(char) * (letters));
 	if (!buf)
 		return (0);
-	rd = read(fd, buf, letters);
-	wr = write(STDOUT_FILENO, buf, rd);
+
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
+
 	close(fd);
+
 	free(buf);
-	return (wr);
+
+	return (nwr);
 }
