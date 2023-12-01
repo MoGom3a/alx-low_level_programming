4c76a28cf25068d4242bf1c7bb09f863c5eb50c1
Mohamed <ph.dude.95@gmail.com>
2023-12-01 04:43:58 +0200

    0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,7 +13,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t nrd, nwr;
 	char *buf;
 
-	if (!filename)
+	if (!filename || !letters)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
